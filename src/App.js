b3c4c5cc0984d1f7f83d95845426794b34f987d1
Salvador Hernandez <hernandezg.sal@gmail.com>
2021-07-06 23:28:06 -0700

    Add confetti

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import Item from './item'
+import Confetti from './Confetti';
 import './App.css';
 
+function onClickHandler(event){
+    alert('something')
+}
 function App() {
     return (
         <div className="App">
+            <Item/>
+            <Confetti/>
             <div class="container grid-container">
                 <div class="row">
-                    <div class="col-sm-offset-3 col-sm-1 grid-item">Someone drinks water</div>
+                    <div onClick={event => onClickHandler()} class="col-sm-offset-3 col-sm-1 grid-item">Someone drinks water</div>
                     <div class="col-sm-1 grid-item">Talking way too loud</div>
                     <div class="col-sm-1 grid-item">Someone is late</div>
                     <div class="col-sm-1 grid-item">Ticket Rolls</div>
