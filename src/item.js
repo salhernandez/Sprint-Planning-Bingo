0b3a1a58f2abd2c50380b28147abe127ce858347
Sal Hernandez <hernandezg.sal@gmail.com>
2021-07-11 13:56:10 -0800

    Add rer-mark element and styles

--- src/item.js
+++ src/item.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import img from './mark.png';
 
 export default class GridItem extends React.Component {
   render() {
     return (
       <div class="row">
-        <div onClick={() => this.props.onClickHandler()} class="col-sm-offset-3 col-sm-1 grid-item">{this.props.labels[0]}</div>
+        <div 
+          onClick={() => this.props.onClickHandler()} 
+          class="col-sm-offset-3 col-sm-1 grid-item">
+            {this.props.labels[0]}
+            <img src={img} className="red-mark"/>
+          </div>
         <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[1]}</div>
         <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[2]}</div>
         <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[3]}</div>
