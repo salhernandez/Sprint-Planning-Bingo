71729ddb9c5e8e8b71a12f4c4d97c3e44f735525
Sal Hernandez <hernandezg.sal@gmail.com>
2021-07-11 14:03:09 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -3,51 +3,56 @@ import Item from './item';
 import Confetti from './Confetti';
 import './App.css';
 
-const rowData = [
-    [
-        "Someone drinks water",
-        "Talking way too loud",
-        "Someone is late",
-        "Ticket Rolls",
-        "Annoying Echo"
-    ],
-    [
-        "Upset people or funny face",
-        "Forehead only shown",
-        "Breathing aggresively",
-        "Talking on mute",
-        "Timeline/Status Update"
-    ],
-    [
-        "Talking over each other",
-        "Not sharing screen",
-        "Free",
-        ">13 Story points",
-        "Eating/Snacking"
-    ],
-    [
-        "Someone leaves early",
-        "Awkward silence",
-        "Animal distraction",
-        "Robot/Static Voice",
-        "Burndown is a cliff dive"
-    ],
-    [
-        "Asking if everyone can hear you",
-        "Going off topic",
-        "<45 min. or >4hr sprint plannning",
-        "Background talking/noise",
-        "Covering mouth with hand"
-    ]
-]
-
 export default class App extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            rowData : [
+                [
+                    "Someone drinks water",
+                    "Talking way too loud",
+                    "Someone is late",
+                    "Ticket Rolls",
+                    "Annoying Echo"
+                ],
+                [
+                    "Upset people or funny face",
+                    "Forehead only shown",
+                    "Breathing aggresively",
+                    "Talking on mute",
+                    "Timeline/Status Update"
+                ],
+                [
+                    "Talking over each other",
+                    "Not sharing screen",
+                    "Free",
+                    ">13 Story points",
+                    "Eating/Snacking"
+                ],
+                [
+                    "Someone leaves early",
+                    "Awkward silence",
+                    "Animal distraction",
+                    "Robot/Static Voice",
+                    "Burndown is a cliff dive"
+                ],
+                [
+                    "Asking if everyone can hear you",
+                    "Going off topic",
+                    "<45 min. or >4hr sprint plannning",
+                    "Background talking/noise",
+                    "Covering mouth with hand"
+                ]
+            ]
+        }
+    }
     onClickHandler(event) {
         alert('something')
     }
 
     render(){
+        const {rowData} = this.state;
         return (
             <div className="App">
                 <Confetti />
