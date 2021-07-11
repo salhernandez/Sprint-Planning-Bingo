3b82845a60e0a48d89e6370b6527bc8b7d1853e8
Sal Hernandez <hernandezg.sal@gmail.com>
2021-07-11 13:24:04 -0800

    Create array of strings

--- src/App.js
+++ src/App.js
@@ -2,65 +2,59 @@ import Item from './item'
 import Confetti from './Confetti';
 import './App.css';
 
-function onClickHandler(event){
+function onClickHandler(event) {
     alert('something')
 }
+
+const rowData = [
+    [
+        "Someone drinks water",
+        "Talking way too loud",
+        "Someone is late",
+        "Ticket Rolls",
+        "Annoying Echo"
+    ],
+    [
+        "Upset people or funny face",
+        "Forehead only shown",
+        "Breathing aggresively",
+        "Talking on mute",
+        "Timeline/Status Update"
+    ],
+    [
+        "Talking over each other",
+        "Not sharing screen",
+        "Free",
+        ">13 Story points",
+        "Eating/Snacking"
+    ],
+    [
+        "Someone leaves early",
+        "Awkward silence",
+        "Animal distraction",
+        "Robot/Static Voice",
+        "Burndown is a cliff dive"
+    ],
+    [
+        "Asking if everyone can hear you",
+        "Going off topic",
+        "<45 min. or >4hr sprint plannning",
+        "Background talking/noise",
+        "Covering mouth with hand"
+    ]
+]
 
 function App() {
     return (
         <div className="App">
-            <Confetti/>
+            <Confetti />
             <div class="container grid-container">
-                <Item
-                    onClickHandler = {onClickHandler}
-                    labels = {[
-                        "Someone drinks water",
-                        "Talking way too loud",
-                        "Someone is late",
-                        "Ticket Rolls",
-                        "Annoying Echo"
-                    ]}
-                />
-                <Item
-                    onClickHandler = {onClickHandler}
-                    labels = {[
-                        "Upset people or funny face",
-                        "Forehead only shown",
-                        "Breathing aggresively",
-                        "Talking on mute",
-                        "Timeline/Status Update"
-                    ]}
-                />
-                <Item
-                    onClickHandler = {onClickHandler}
-                    labels = {[
-                        "Talking over each other",
-                        "Not sharing screen",
-                        "Free",
-                        ">13 Story points",
-                        "Eating/Snacking"
-                    ]}
-                />
-                <Item
-                    onClickHandler = {onClickHandler}
-                    labels = {[
-                        "Someone leaves early",
-                        "Awkward silence",
-                        "Animal distraction",
-                        "Robot/Static Voice",
-                        "Burndown is a cliff dive"
-                    ]}
-                />
-                <Item
-                    onClickHandler = {onClickHandler}
-                    labels = {[
-                        "Asking if everyone can hear you",
-                        "Going off topic",
-                        "<45 min. or >4hr sprint plannning",
-                        "Background talking/noise",
-                        "Covering mouth with hand"
-                    ]}
-                />
+                {rowData.map(row =>
+                    <Item
+                        onClickHandler={onClickHandler}
+                        labels={row}
+                    />
+                )}
             </div>
         </div>
     );
