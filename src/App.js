cabbeba2e77b79cecd2d8c37b131b7766acb8d70
Sal Hernandez <hernandezg.sal@gmail.com>
2021-07-11 13:18:22 -0800

    Use reusable component

--- src/App.js
+++ src/App.js
@@ -21,34 +21,46 @@ function App() {
                         "Annoying Echo"
                     ]}
                 />
-                <div class="row">
-                    <div class="col-sm-offset-3 col-sm-1 grid-item">Upset people or funny face</div>
-                    <div class="col-sm-1 grid-item">Forehead only shown</div>
-                    <div class="col-sm-1 grid-item">Breathing aggresively</div>
-                    <div class="col-sm-1 grid-item">Talking on mute</div>
-                    <div class="col-sm-1 grid-item">Timeline/Status Update</div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-offset-3 col-sm-1 grid-item">Talking over each other</div>
-                    <div class="col-sm-1 grid-item">Not sharing screen</div>
-                    <div class="col-sm-1 grid-item">Free</div>
-                    <div class="col-sm-1 grid-item"> {'>13 Story points'}</div>
-                    <div class="col-sm-1 grid-item"> Eating/Snacking</div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-offset-3 col-sm-1 grid-item">Someone leaves early</div>
-                    <div class="col-sm-1 grid-item">Awkward silence</div>
-                    <div class="col-sm-1 grid-item">Animal distraction</div>
-                    <div class="col-sm-1 grid-item">Robot/Static Voice</div>
-                    <div class="col-sm-1 grid-item">Burndown is a cliff dive</div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-offset-3 col-sm-1 grid-item">Asking if everyone can hear you</div>
-                    <div class="col-sm-1 grid-item">Going off topic</div>
-                    <div class="col-sm-1 grid-item"> {'<45 min. or >4hr sprint plannning'}</div>
-                    <div class="col-sm-1 grid-item">Background talking/noise</div>
-                    <div class="col-sm-1 grid-item">Covering mouth with hand</div>
-                </div>
+                <Item
+                    onClickHandler = {onClickHandler}
+                    labels = {[
+                        "Upset people or funny face",
+                        "Forehead only shown",
+                        "Breathing aggresively",
+                        "Talking on mute",
+                        "Timeline/Status Update"
+                    ]}
+                />
+                <Item
+                    onClickHandler = {onClickHandler}
+                    labels = {[
+                        "Talking over each other",
+                        "Not sharing screen",
+                        "Free",
+                        ">13 Story points",
+                        "Eating/Snacking"
+                    ]}
+                />
+                <Item
+                    onClickHandler = {onClickHandler}
+                    labels = {[
+                        "Someone leaves early",
+                        "Awkward silence",
+                        "Animal distraction",
+                        "Robot/Static Voice",
+                        "Burndown is a cliff dive"
+                    ]}
+                />
+                <Item
+                    onClickHandler = {onClickHandler}
+                    labels = {[
+                        "Asking if everyone can hear you",
+                        "Going off topic",
+                        "<45 min. or >4hr sprint plannning",
+                        "Background talking/noise",
+                        "Covering mouth with hand"
+                    ]}
+                />
             </div>
         </div>
     );
