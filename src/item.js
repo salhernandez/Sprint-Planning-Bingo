1ea93b694e102a81472587a757fde98f83eceb27
Sal Hernandez <hernandezg.sal@gmail.com>
2021-07-11 13:28:06 -0800

    Remove unused paramter

--- src/item.js
+++ src/item.js
@@ -4,11 +4,11 @@ export default class GridItem extends React.Component {
   render() {
     return (
       <div class="row">
-        <div onClick={event => this.props.onClickHandler()} class="col-sm-offset-3 col-sm-1 grid-item">{this.props.labels[0]}</div>
-        <div onClick={event => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[1]}</div>
-        <div onClick={event => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[2]}</div>
-        <div onClick={event => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[3]}</div>
-        <div onClick={event => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[4]}</div>
+        <div onClick={() => this.props.onClickHandler()} class="col-sm-offset-3 col-sm-1 grid-item">{this.props.labels[0]}</div>
+        <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[1]}</div>
+        <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[2]}</div>
+        <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[3]}</div>
+        <div onClick={() => this.props.onClickHandler()} class="col-sm-1 grid-item">{this.props.labels[4]}</div>
       </div>
     )
   }
